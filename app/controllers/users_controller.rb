3e3a42c97b4d05973ab1ee14aa916252b219dcf2
Collin Jilbert <cjilbert504@gmail.com>
2020-08-08 09:10:17 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,52 @@
 class UsersController < ApplicationController
-
-    get '/signup' do
-        if Helpers.is_logged_in?(session) 
-          redirect to '/tweets'
-        end
-        erb :'/users/signup'
+    get '/users/:slug' do
+      @user = User.find_by_slug(params[:slug])
+      erb :'users/show'
     end
-    
+  
+    get '/signup' do
+      if !logged_in?
+        erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+      else
+        redirect to '/tweets'
+      end
+    end
+  
     post '/signup' do
-        if params[:username].empty? || params[:email].empty? || params[:password].empty?
-          redirect to '/signup'
-        end
-        @user = User.create(params)
+      if params[:username] == "" || params[:email] == "" || params[:password] == ""
+        redirect to '/signup'
+      else
+        @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+        @user.save
         session[:user_id] = @user.id
         redirect to '/tweets'
+      end
     end
-
+  
     get '/login' do
-        if Helpers.is_logged_in?(session)
-            redirect to '/tweets'
-        end
-        erb :'/users/login'
+      if !logged_in?
+        erb :'users/login'
+      else
+        redirect to '/tweets'
+      end
     end
-
+  
     post '/login' do
-        @user = User.find_by(:username => params[:username])
-        if @user && @user.authenticate(params[:password])
-            session[:user_id] = @user.id
-            redirect '/tweets'
-        end
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect to "/tweets"
+      else
+        redirect to '/signup'
+      end
     end
-
-end
+  
+    get '/logout' do
+      if logged_in?
+        session.destroy
+        redirect to '/login'
+      else
+        redirect to '/'
+      end
+    end
+  end
