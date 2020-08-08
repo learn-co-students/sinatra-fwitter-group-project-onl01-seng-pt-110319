c9fffef2b58100eea919b714dc164302117df0be
Stephen_Sanchez <Stephensanchez05@gmail.com>
2020-08-07 22:46:25 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,9 @@
 class UsersController < ApplicationController
     get '/users/:slug' do
-      @user = User.find_by_slug(params[:slug])
+      @tweeter = User.find_by_slug(params[:slug])
       erb :'users/show'
     end
   
-    get '/signup' do
-      if !logged_in?
-        erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
-      else
-        redirect to '/tweets'
-      end
-    end
-  
-    post '/signup' do
-      if params[:username] == "" || params[:email] == "" || params[:password] == ""
-        redirect to '/signup'
-      else
-        @users = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-        @users.save
-        session[:user_id] = @users.id
-        redirect to '/tweets'
-      end
-    end
   
     get '/login' do
       if !logged_in?
@@ -32,9 +14,9 @@ class UsersController < ApplicationController
     end
   
     post '/login' do
-      tweeter = User.find_by(:username => params[:username])
-      if tweeter && tweeter.authenticate(params[:password])
-        session[:user_id] = tweeter.id
+      user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
         redirect to "/tweets"
       else
         redirect to '/signup'
@@ -49,4 +31,23 @@ class UsersController < ApplicationController
         redirect to '/'
       end
     end
+
+    get '/signup' do
+        if !logged_in?
+          erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+        else
+          redirect to '/tweets'
+        end
+      end
+    
+      post '/signup' do
+        if params[:username] == "" || params[:email] == "" || params[:password] == ""
+          redirect to '/signup'
+        else
+          @tweeter  = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+          @tweeter .save
+          session[:user_id] = @tweeter .id
+          redirect to '/tweets'
+        end
+      end
   end
